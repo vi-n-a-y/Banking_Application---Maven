83aa824bf14dd1ad28160a6799f31691ae591e59
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-03-08 23:12:30 +0530

    Create Navbar.jsx

---
+++ banking_application-react/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+const Navbar=()=>{
+    return (
+<>
+<nav>
+    <ul id="menuList">
+        <li><a href="#" className="nav-link">Home</a></li>
+        <li><a href="#" className="nav-link">Jobs</a></li>
+        <li><a href="#" className="nav-link">Contact Us</a></li>
+        <li><a href="#" className="nav-link">About Us</a></li>
+
+
+    </ul>
+</nav>
+
+</>
+
+     )
+}
+export default Navbar
