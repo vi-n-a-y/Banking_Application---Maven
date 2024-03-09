0af2139e2e88edebe18ac15a00a8d2ee9b613ab8
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-03-09 23:20:08 +0530

    Update Navbar.jsx

--- banking_application-react/src/components/Navbar.jsx
+++ banking_application-react/src/components/Navbar.jsx
@@ -1,19 +1,49 @@
-const Navbar=()=>{
-    return (
-<>
-<nav>
-    <ul id="menuList">
-        <li><a href="#" className="nav-link">Home</a></li>
-        <li><a href="#" className="nav-link">Jobs</a></li>
-        <li><a href="#" className="nav-link">Contact Us</a></li>
-        <li><a href="#" className="nav-link">About Us</a></li>
 
+import React, { useState } from 'react';
+import '../App.css';
 
-    </ul>
-</nav>
+const Nav = () => {
+  const [initialPosition, setInitialPosition] = useState("75%");
 
-</>
+  const highlightLink = (index, end) => {
+    const percentages = ["75%", "50%", "25%", "0"];
+    let targetPosition = index !== -1 ? percentages[index] : end;
 
-     )
-}
-export default Navbar
+    setInitialPosition(targetPosition);
+    setTimeout(() => {
+      document.getElementById("menuList").classList.add("clicked");
+    }, 0);
+  };
+
+  const handleLinkClick = (index) => {
+    highlightLink(index);
+  };
+
+  const handleKeyPress = (event) => {
+    let start = +initialPosition.slice(0, 2) || 0;
+    let end;
+
+    if (event.key === "ArrowRight") {
+      end = start !== 0 ? start - 25 : 0;
+    }
+    if (event.key === "ArrowLeft") {
+      end = start !== 75 ? start + 25 : 75;
+    }
+
+    highlightLink(-1, end + "%");
+  };
+
+  return (
+    <nav>
+      <ul id="menuList" onKeyDown={handleKeyPress}>
+        <li><a href="#" className="nav-link" onClick={() => handleLinkClick(0)}>Home</a></li>
+        <li><a href="#" className="nav-link" onClick={() => handleLinkClick(1)}>Products</a></li>
+        <li><a href="#" className="nav-link" onClick={() => handleLinkClick(2)}>Contact Us</a></li>
+        <li><a href="#" className="nav-link" onClick={() => handleLinkClick(3)}>About Us</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
+
