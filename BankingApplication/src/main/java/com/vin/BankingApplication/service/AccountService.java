4b5b64e886e21369875b5a03314c71d1906b60a4
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-03-12 23:09:32 +0530

    Update AccountService.java

--- BankingApplication/src/main/java/com/vin/BankingApplication/service/AccountService.java
+++ BankingApplication/src/main/java/com/vin/BankingApplication/service/AccountService.java
@@ -5,28 +5,14 @@ import java.util.List;
 import com.vin.BankingApplication.model.Account;
 import com.vin.BankingApplication.model.Transaction;
 
-
 public interface AccountService {
-	
-	
-	
+
 	Account addAccountDetails(Account account);
 
 	Account createAccount(Account account);
 
-
-
-	Account addAmountToAcc(Long id, Double amount);
-
-	Account sendMoney(Long id, Double amount);
-
 	Account getAccountById(Long id);
 
 	List<Transaction> getUserTransaction(Long id);
-
-	Account setUserTransaction(Long id, List<Transaction> transactions); 
-	
-	//Account getAccountDetails(Long id);
-	//Account getAccountDetails(String accNmbr);
 
 }
