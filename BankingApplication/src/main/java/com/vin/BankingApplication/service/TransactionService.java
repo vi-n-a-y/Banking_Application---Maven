b0af95a92c6b5b47333b6568bdfcd318d9ac5a89
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-03-12 23:02:21 +0530

    Update TransactionService.java

--- BankingApplication/src/main/java/com/vin/BankingApplication/service/TransactionService.java
+++ BankingApplication/src/main/java/com/vin/BankingApplication/service/TransactionService.java
@@ -2,10 +2,20 @@ package com.vin.BankingApplication.service;
 
 import java.util.List;
 
+import com.vin.BankingApplication.model.Account;
 import com.vin.BankingApplication.model.Transaction;
 
 public interface TransactionService {
 
-	
+	Transaction setUserTransaction(Account fromAccount, Account toAccount, String accType, double amount,
+			String description);
+
+	Account addAmountToAcc(Long id, Double amount);
+
+	Account sendMoney(Long id, long id1, Double amount);
+
+	Transaction setAddTransaction(Account fromAccount, double amount);
+
+	List<Transaction> generateStatement(Account account, String startDate, String endDate);
 
 }
