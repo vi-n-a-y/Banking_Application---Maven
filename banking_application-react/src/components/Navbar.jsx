d6a76e826a253b52f6ce141c1449e890a6e33f04
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-03-19 16:49:33 +0530

    Update Navbar.jsx

--- banking_application-react/src/components/Navbar.jsx
+++ banking_application-react/src/components/Navbar.jsx
@@ -1,21 +1,34 @@
-import React, { useState } from 'react';
-import '../index.css';
+import React, { useState } from "react";
+import "../index.css";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-
+  const handleLogout = () => {};
 
   return (
-  <nv>
-    <div className="bg_black">
-        <b><a className="logo1" href="#">Self B@/\/k</a></b>
-        <a className="nav" href="#">Menu</a>
-        <a className="nav" href="" >Jobs</a>
-        <a className="nav" href="" >Contact Us</a>     
+    <nav>
+      <div className="bg_black">
+        <b>
+          <a className="logo1" href="#">
+            Self B@/\/k
+          </a>
+        </b>
+        <a className="nav" href="#">
+          Menu
+        </a>
+        <a className="nav" href="">
+          Jobs
+        </a>
+        <a className="nav" href="">
+          Contact Us
+        </a>
         <input type="text" placeholder="search" />
         <button className="search">Search</button>
-        <a href=""><button className="sign">Logout</button></a>
-    </div>
-  </nv>
+        <button className="sign" onClick={handleLogout}>
+          Logout
+        </button>
+      </div>
+    </nav>
   );
 };
 
